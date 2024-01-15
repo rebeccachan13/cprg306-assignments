a3820760f9e267e704f922cf1f48434937c391c8
Rebecca Chan <125217883+rebeccachan13@users.noreply.github.com>
2024-01-15 15:29:49 -0700

    Created week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Rebecca Oi-ling Chan</p>
+      
+      <Link href="https://github.com/rebeccachan13">https://github.com/rebeccachan13</Link>
+    </div>
+  );
+}
