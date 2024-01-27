7c0a54145cb4369ad6199846133be792ba36f08d
Rebecca Chan <125217883+rebeccachan13@users.noreply.github.com>
2024-01-26 18:33:51 -0700

    Create week 3 assignment

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="bg-gray-950 text-gray-50 p-2">
+      <h1 className="text-3xl font-bold">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
