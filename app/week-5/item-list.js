153a5580452de7b488a3e72f0f40f37ecd70a7a8
Rebecca Chan <125217883+rebeccachan13@users.noreply.github.com>
2024-02-23 19:31:41 -0700

    Update Week 5 Assignment

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -6,7 +6,6 @@ import items from './items.json';
 
 export default function ItemList() {
     const [sortBy, setSortBy] = useState('name');
-    const [grouped, setGrouped] = useState(false);
 
     const sortedName = items.sort((a, b) => {
         if (sortBy === 'name') {
@@ -18,49 +17,16 @@ export default function ItemList() {
         }
     });
 
-    const groupedItems = () => {
-        const groupedData = sortedName.reduce((result, item) => {
-            if (!result[item.category]) {
-                result[item.category] = [];
-            }
-            result[item.category].push(item);
-            return result;
-        }, {});
-
-        return Object.entries(groupedData).sort((a, b) => a[0].localeCompare(b[0]));
-    };
-
-    const handleGroupButtonClick = () => {
-        setGrouped(!grouped);
-    };
-
       return (
         <>
             <div>
                 <label>Sort By:</label>
                 <button onClick={() => setSortBy('name')} className={`p-1 m-2 w-32 ${sortBy === 'name' ? 'bg-orange-500' : 'bg-orange-700'}`}>Name</button>
-                <button onClick={() => setSortBy('category')} className={`p-1 m-2 w-32 ${sortBy === 'category' ? 'bg-orange-500' : 'bg-orange-700'}`}>Category</button>  
-                <button onClick={handleGroupButtonClick} className={`p-1 m-2 w-32 ${grouped ? 'bg-orange-500' : 'bg-orange-700'}`}>Grouped Category</button>       
+                <button onClick={() => setSortBy('category')} className={`p-1 m-2 w-32 ${sortBy === 'category' ? 'bg-orange-500' : 'bg-orange-700'}`}>Category</button>    
                 
             </div>
             <ul>
-                {grouped
-                    ? groupedItems().map(([category, items]) => (
-                          <li key={category}>
-                              <h3 className="text-xl capitalize">{category}</h3>
-                              <ul>
-                                  {items.map((item) => (
-                                      <Item
-                                          key={item.id}
-                                          name={item.name}
-                                          quantity={item.quantity}
-                                          category={item.category}
-                                      />
-                                  ))}
-                              </ul>
-                          </li>
-                      ))
-                      : sortedName.map((item) => (
+                {sortedName.map((item) => (
                         <Item 
                         key={item.id} 
                         name={item.name} 
